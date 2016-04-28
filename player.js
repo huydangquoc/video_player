ef209354cdd6baf66d552b184aa25447d71aba53
huydangquoc <huysamdua@yahoo.com>
2016-04-28 12:12:38 +0700

    Add time field function

--- player.js
+++ player.js
@@ -9,11 +9,13 @@ window.addEventListener('load', function() {
 
 	// buttons container
 	playButton = document.getElementById('play-button');
+	timeField = document.getElementById('time-field');
 
 	video.load();
 	video.addEventListener('canplay', function() {
 		playButton.addEventListener('click', playOrPause, false);
 		pbarContainer.addEventListener('click', skip, false);
+		updatePlayer();
 
 	}, false);
 
@@ -34,6 +36,8 @@ function playOrPause() {
 function updatePlayer() {
 	var percentage = (video.currentTime/video.duration)*100;
 	pbar.style.width = percentage + '%';
+	timeField.innerHTML = getFormattedTime();
+
 	if (video.ended) {
 		window.clearInterval(update);
 		playButton.src = 'images/replay.png';
@@ -47,4 +51,19 @@ function skip(ev) {
 
 	video.currentTime = (mouseX/width)*video.duration;
 	updatePlayer();
+}
+
+function getFormattedTime() {
+	// 1:32
+	var seconds = Math.round(video.currentTime);
+	var minutes = Math.floor(seconds/60);
+	if(minutes > 0) seconds -= minutes*60;
+	if(seconds.toString().length === 1) seconds = '0' + seconds;
+
+	var totalSeconds = Math.round(video.duration);
+	var totalMinutes = Math.floor(totalSeconds/60);
+	if(totalMinutes > 0) totalSeconds -= totalMinutes*60;
+	if(totalSeconds.toString().length === 1) seconds = '0' + totalSeconds;
+
+	return minutes + ':' + seconds + ' / ' + totalMinutes + ':' + totalSeconds;
 }
